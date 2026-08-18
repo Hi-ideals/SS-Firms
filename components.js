ee48387878cff790065eb492c4c609b24c794ab0
Hi-ideals <126317624+Hi-ideals@users.noreply.github.com>
2026-08-18 16:09:29 +0530

    Update components.js

--- components.js
+++ components.js
@@ -128,20 +128,16 @@ function buildFooter() {
             <div class="footer-contact-item">
               <i class="fas fa-envelope"></i>
               <span>${SITE.subpageAddress.email}</span>
-            </div>
-            
-            <div class="footer-contact-item">
               <i class="fas fa-map-marker-alt"></i>
               <span>${SITE.subpageAddress.usa1}</span>
-            </div>
-            <div class="footer-contact-item">
               <i class="fas fa-phone-alt"></i>
               <span>${SITE.subpageAddress.phone2}</span>
-            </div>
-            <div class="footer-contact-item">
               <i class="fas fa-envelope"></i>
               <span>${SITE.subpageAddress.email1}</span>
             </div>
+            
+           
+              
             
           </div>
 
