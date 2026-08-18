c9bc19975c69189a1027a5351ddca2775d092273
Hi-ideals <126317624+Hi-ideals@users.noreply.github.com>
2026-08-18 16:07:13 +0530

    Refactor footer contact section layout
    Removed unnecessary div elements in footer contact section.

--- components.js
+++ components.js
@@ -129,9 +129,7 @@ function buildFooter() {
               <i class="fas fa-envelope"></i>
               <span>${SITE.subpageAddress.email}</span>
             </div>
-          </div>
-
-           <div>
+            
             <div class="footer-contact-item">
               <i class="fas fa-map-marker-alt"></i>
               <span>${SITE.subpageAddress.usa1}</span>
@@ -144,7 +142,9 @@ function buildFooter() {
               <i class="fas fa-envelope"></i>
               <span>${SITE.subpageAddress.email1}</span>
             </div>
+            
           </div>
+
         </div>
         <!-- Footer Bottom -->
         <div class="footer-bottom">
